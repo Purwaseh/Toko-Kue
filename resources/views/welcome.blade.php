99d87c7cd8a9c9d4ee78418282d2a1c33ad94c25
Porwaseh <pasehsep17@gmail.com>
2019-09-23 13:08:22 +0700

    perubahan halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    toko kue
                 </div>
 
                 <div class="links">
