27a3704795cefe28a67db5339fcef945b8c38fb2
yuyun111 <yuyunfitriahningsih@gmail.com>
2019-09-25 10:52:01 +0700

    perubahan 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Toko kue kita
+                  ASEH ISEH MUMET
                 </div>
 
                 <div class="links">
