94c83131f0e0984c147f044a2f8a7d911731cba1
atikafanda <atikafandalestari@gmail.com>
2019-09-25 10:28:37 +0700

    perubahan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Atika Fanda 
+                    Toko kue
                 </div>
 
                 <div class="links">
