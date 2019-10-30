617ad4106e207c23be2167c418d9d618d288eddf
Porwaseh <pasehsep17@gmail.com>
2019-10-30 09:07:19 +0700

    klarifikasi db

---
+++ resources/views/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table border="1">
+        Toko Kue Kita
+    <tr>
+        <td>menu</td>
+        <td>tampilan</td>
+    </tr>
+    <tr>
+        <td>Manajemen Menu</td>
+        <td>tampilan</td>
+    </tr>
+</table>
+</body>
+</html>
